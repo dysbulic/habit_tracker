f2a8eb97162354fcf1ccb7f47bd6634129351740
Will Holcomb <will@dhappy.org>
2013-09-15 11:46:14 -0400

    switching to cursor adapter

--- src/com/synaptian/smoketracker/habits/HabitTimeAdapter.java
+++ src/com/synaptian/smoketracker/habits/HabitTimeAdapter.java
@@ -2,59 +2,111 @@
 
 package com.synaptian.smoketracker.habits;
 
-import java.util.List;
-import android.app.Activity;
+import android.content.Context;
+import android.database.Cursor;
+import android.graphics.Color;
+import android.support.v4.widget.SimpleCursorAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.TextView;
 import org.dhappy.android.widget.Timer;
 
-public class HabitTimeAdapter extends ArrayAdapter {
-	private final Activity activity;
-	private final List<HabitTime> habitTimes;
+import com.synaptian.smoketracker.habits.database.HabitTable;
 
-	public HabitTimeAdapter(Activity activity, List<HabitTime> objects) {
-		super(activity, R.layout.habit_row, objects);
-		this.activity = activity;
-		this.habitTimes = objects;
+public class HabitTimeAdapter extends SimpleCursorAdapter {
+	private int mSelectedPosition;
+	Cursor items;
+	private Context context;
+	private int layout;
+
+	public HabitTimeAdapter(Context context, int layout, Cursor c, String[] from, int[] to, int flags) {
+		super(context, layout, c, from, to, flags);
+		this.context = context;
+		this.layout = layout;
 	}
 
 	@Override
-	public View getView(int position, View convertView, ViewGroup parent) {
-		View rowView = convertView;
-		HabitTimeView htView = null;
+	public View newView(Context context, Cursor cursor, ViewGroup parent) {
+		Cursor c = getCursor();
 
-		if (rowView == null) {
-			// Get a new instance of the row layout view
-			LayoutInflater inflater = activity.getLayoutInflater();
-			rowView = inflater.inflate(R.layout.habit_row, null);
+		final LayoutInflater inflater = LayoutInflater.from(context);
+		View view = inflater.inflate(layout, parent, false);
 
-			// Hold the view objects in an object,
-			// so they don't need to be re-fetched
-			htView = new HabitTimeView();
-			htView.name = (TextView) rowView.findViewById(R.id.label);
-			htView.timer = (Timer) rowView.findViewById(R.id.timer);
+		TextView name = (TextView) view.findViewById(R.id.label);
+		Timer timer = (Timer) view.findViewById(R.id.timer);
 
-			// Cache the view objects in the tag,
-			// so they can be re-accessed later
-			rowView.setTag(htView);
-		} else {
-			htView = (HabitTimeView) rowView.getTag();
-		}
+		name.setText(c.getString(c.getColumnIndex(HabitTable.COLUMN_NAME)));
+		timer.setStartingTime(c.getInt(c.getColumnIndex(HabitTable.COLUMN_TIME)));
 
-		// Transfer the stock data from the data object
-		// to the view objects
-		HabitTime currentTime = habitTimes.get(position);
-		htView.name.setText(currentTime.getName());
-		htView.timer.setStartingTime(currentTime.getTime());
-
-		return rowView;
+		return view;
 	}
 
-	protected static class HabitTimeView {
-		protected TextView name;
-		protected Timer timer;
+
+	@Override
+	public void bindView(View view, Context context, Cursor c) {
+		TextView name = (TextView) view.findViewById(R.id.label);
+		Timer timer = (Timer) view.findViewById(R.id.timer);
+
+		name.setText(c.getString(c.getColumnIndex(HabitTable.COLUMN_NAME)));
+		timer.setStartingTime(c.getInt(c.getColumnIndex(HabitTable.COLUMN_TIME)));
+
+		name.setTextColor(Color.GREEN);
+		view.setBackgroundColor(Color.WHITE);
+	}
+
+	public void setSelectedPosition(int position) {
+		mSelectedPosition = position;
+		notifyDataSetChanged();
 	}
 }
+
+/*
+public class HabitTimeAdapter extends ArrayAdapter {
+private final Activity activity;
+private final List<HabitTime> habitTimes;
+
+public HabitTimeAdapter(Activity activity, List<HabitTime> objects) {
+	super(activity, R.layout.habit_row, objects);
+	this.activity = activity;
+	this.habitTimes = objects;
+}
+
+@Override
+public View getView(int position, View convertView, ViewGroup parent) {
+	View rowView = convertView;
+	HabitTimeView htView = null;
+
+	if (rowView == null) {
+		// Get a new instance of the row layout view
+		LayoutInflater inflater = activity.getLayoutInflater();
+		rowView = inflater.inflate(R.layout.habit_row, null);
+
+		// Hold the view objects in an object,
+		// so they don't need to be re-fetched
+		htView = new HabitTimeView();
+		htView.name = (TextView) rowView.findViewById(R.id.label);
+		htView.timer = (Timer) rowView.findViewById(R.id.timer);
+
+		// Cache the view objects in the tag,
+		// so they can be re-accessed later
+		rowView.setTag(htView);
+	} else {
+		htView = (HabitTimeView) rowView.getTag();
+	}
+
+	// Transfer the stock data from the data object
+	// to the view objects
+	HabitTime currentTime = habitTimes.get(position);
+	htView.name.setText(currentTime.getName());
+	htView.timer.setStartingTime(currentTime.getTime());
+
+	return rowView;
+}
+
+protected static class HabitTimeView {
+	protected TextView name;
+	protected Timer timer;
+}
+}
+*/
--- src/com/synaptian/smoketracker/habits/HabitTimeListActivity.java
+++ src/com/synaptian/smoketracker/habits/HabitTimeListActivity.java
@@ -2,23 +2,52 @@ package com.synaptian.smoketracker.habits;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import com.synaptian.smoketracker.habits.contentprovider.MyHabitContentProvider;
+import com.synaptian.smoketracker.habits.database.HabitTable;
+
 import android.app.ListActivity;
+import android.app.LoaderManager;
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.widget.SimpleCursorAdapter;
 
-public class HabitTimeListActivity extends ListActivity {
-
+public class HabitTimeListActivity extends ListActivity implements LoaderManager.LoaderCallbacks<Cursor> {
+	private SimpleCursorAdapter adapter;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.event_list);
 
-        List habitTimes = new ArrayList();
-        habitTimes.add(new HabitTime("MSFT", 24 * 1000));
-        habitTimes.add(new HabitTime("ORCL", 34 * 1000));
-        habitTimes.add(new HabitTime("AMZN", 180 * 1000));
-        habitTimes.add(new HabitTime("ERTS", 19 * 1000));
+        String[] from = new String[] { HabitTable.COLUMN_NAME, HabitTable.COLUMN_TIME };
+        // Fields on the UI to which we map
+        int[] to = new int[] { R.id.label, R.id.timer };
 
-        setListAdapter(new HabitTimeAdapter(this, habitTimes));
+        getLoaderManager().initLoader(0, null, this);
+        adapter = new SimpleCursorAdapter(this, R.layout.habit_row, null, from, to, 0);
+
+        setListAdapter(adapter);
+    }
+    
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        String[] projection = { HabitTable.COLUMN_ID, HabitTable.COLUMN_NAME };
+        CursorLoader cursorLoader = new CursorLoader(this,
+            MyHabitContentProvider.CONTENT_URI, projection, null, null, null);
+        return cursorLoader;
+    }
+    
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+      adapter.swapCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+      // data is not available anymore, delete reference
+      adapter.swapCursor(null);
     }
 }
