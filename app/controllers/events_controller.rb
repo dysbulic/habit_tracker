ecab6ee22d6c2afe01e71d6e5602397b0f13e23e
U-fenrir\will <will@fenrir.(none)>
2013-11-13 11:14:17 -0500

    sorting events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,7 +6,7 @@ class EventsController < ApplicationController
   # GET /events
   # GET /events.json
   def index
-    @events = Event.all
+    @events = Event.all.sort_by{|e| e.created_at}
   end
 
   # GET /events/1
