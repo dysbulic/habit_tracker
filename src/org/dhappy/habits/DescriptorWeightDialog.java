7c4ab226cc343b689d9407ff4d7c1078395c9569
Will Holcomb <will@dhappy.org>
2014-01-24 14:41:20 -0500

    setting slider defaults

--- src/org/dhappy/habits/DescriptorWeightDialog.java
+++ src/org/dhappy/habits/DescriptorWeightDialog.java
@@ -5,12 +5,38 @@ import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.TextView;
 
 public class DescriptorWeightDialog extends DialogFragment {
 	@Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        builder.setMessage(R.string.habit_edit_cancel)
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View view = inflater.inflate(R.layout.weight_dialog, null);
+        
+        final TextView weight = (TextView) view.findViewById(R.id.weight);
+        SeekBar weightSelect = (SeekBar) view.findViewById(R.id.weight_select);
+        weightSelect.setMax(200);
+        weightSelect.setProgress(100);
+        weightSelect.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                weight.setText(Integer.toString(progress - 100));
+            }
+
+			@Override
+			public void onStartTrackingTouch(SeekBar arg0) {}
+
+			@Override
+			public void onStopTrackingTouch(SeekBar arg0) {}
+        });
+        
+        builder.setView(view)
+               .setMessage(R.string.habit_edit_cancel)
                .setPositiveButton(R.string.weight_confirm, new DialogInterface.OnClickListener() {
                    public void onClick(DialogInterface dialog, int id) {
                        // FIRE ZE MISSILES!
