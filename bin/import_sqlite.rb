73b09910984a5716764fced058c289029918b96e
Will Holcomb <will@dhappy.org>
2014-06-15 06:30:03 -0400

    loading events

--- bin/import_sqlite.rb
+++ bin/import_sqlite.rb
@@ -9,31 +9,34 @@ require 'couchbase'
 sql = SQLite3::Database.new 'data/org.dhappy.habits.db'
 couch = Couchbase.connect bucket: 'habits', host: 'localhost'
 
-statement = sql.prepare 'SELECT * FROM habit'
-res = statement.execute 
-    
+res = sql.prepare('SELECT * FROM habit').execute 
+
+habits = {}
+
 res.each do |row|
-  key = "habit:#{row[0]}:#{Time.now.to_i}"
-  db_obj = couch.get( key, quiet: true )
+  key = "habit:#{row[1].downcase}"
+  habit = couch.get( key, quiet: true )
 
-  puts key
-  next
+  if not habit
+    puts "Creating habit in Couch: #{key}"
+    habit = {
+      type: 'habit',
+      name: row[1],
+      color: row[2]
+    }
 
-  if not db_obj
-    puts "Creating reading in Couch: #{volume.well.name} @ #{volume.mcf} on #{volume.start_date}"
-    save_response = @db.set( key,
-                             {
-                               type: 'reading',
-                               well: db_well['id'],
-                               rails_id: volume.id,
-                               time: volume.start_date,
-                               mcf: volume.mcf
-                             } )
-    
-    db_well['readings'] = db_well['readings'] || []
-    db_well['readings'].push(key)
-    save_response = @db.set(db_well['id'], db_well)
+    save_response = couch.set( key, habit )
   end
+  
+  habits[row[0]] = habit
+end
 
-  puts row.join "\s"
+puts habits
+
+res = sql.prepare('SELECT * FROM event').execute 
+res.each do |row|
+  if habits[row[1]]
+    key = "event:#{habits[row[1]]['name'].downcase}:#{Time.at row[2]}"
+    puts key
+  end
 end
