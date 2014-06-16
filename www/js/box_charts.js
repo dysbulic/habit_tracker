cfc4478ab211f1a11f7120c66b23156db1f5b47b
Will Holcomb <will@dhappy.org>
2014-06-15 21:31:36 -0400

    processing data

--- www/js/box_charts.js
+++ www/js/box_charts.js
@@ -1,6 +1,20 @@
 $( function() {
-    $.getJSON( 'http://localhost/db/habits/_all_docs',
+    $.getJSON( 'http://localhost/db/habits/_design/habit/_view/all',
                function( data ) {
-                   console.log( data )
+                   var habits = {}
+
+                   data.rows
+                       .filter( function( d ) { return d.key == 'habit' } )
+                       .forEach( function( d ) {
+                           habits[d.id] = d.value
+                           habits[d.id].events ||= []
+                       } )
+
+                   data.rows
+                       .filter( function( d ) { return d.key == 'event' } )
+                       .forEach( function( d ) { habits[d.value.habit].events.push( d.value ) } )
+                   
+                   console.log( habits )
+                   
                } )
 } )
